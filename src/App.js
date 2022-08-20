633a3e4c70a415de977a42f15a1711ca0c45171b
Vinh Hoang Ngoc <vinhhoangngoc22@gmail.com>
2022-08-20 12:36:36 +0700

    add sass, navlink

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
+import './App.scss';
 import Header from './components/Header/Header';
-import { Link } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 const App = () => {
   return (
     <div className="app-container">
-      <Header/>
-      <div>
-        test link to test
-        <div>
-          <button>
-            <Link to="/users">Go to user page</Link>
-          </button>
-          <button>
-            <Link to="/admin">Go to Admin page</Link>
-          </button>
+      <div className="header-container">
+        <Header/>
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container">
 
+        </div>
+
+        <div className="app-content">
+          <Outlet/>
         </div>
       </div>
     </div>
