8d9617fb232c1ec828c7d80d3d90ecc2e7db415f
Vinh Hoang Ngoc <vinhhoangngoc22@gmail.com>
2022-08-21 23:24:17 +0700

    Upload File

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,70 +1,114 @@
-import React, { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import { FaPlusCircle } from 'react-icons/fa'
 
 const ModalCreateUser = () => {
-    const [show, setShow] = useState(false);
+  const [show, setShow] = useState(false)
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [role, setRole] = useState('USER')
+  const [image, setImage] = useState('')
+  const [previewImage, setPreviewImage] = useState('')
 
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
+  const handleUploadImage = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setPreviewImage(URL.createObjectURL(event.target.files[0]))
+      setImage(event.target.files[0])
+    }
+  }
 
-            <Modal 
-            show={show} 
-            onHide={handleClose} 
-            size="xl"
-            static
-            className="modal-add-user">
-                <Modal.Header closeButton>
-                    <Modal.Title>Add New User</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form className="row g-3">
-                        <div className="col-md-6">
-                            <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
-                        </div>
-                        <div className="col-md-6">
-                            <label fclassName="form-label">Password</label>
-                            <input type="password" className="form-control" />
-                        </div>
-                        <div className="col-md-6">
-                            <label className="form-label">Username</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col-md-4">
-                            <label className="form-label">Role</label>
-                            <select className="form-select">
-                                <option selected value="USER">Role</option>
-                                <option value="ADMIN">ADMIN</option>
-                            </select>
-                        </div>
-                        <div className="col-md-12">
-                            <label className="form-label">Upload File Image</label>
-                            <input type="file" hidden/>
-                            <div className="col-md-12 img-preview">
-                                {/* <img src="" alt="#" /> */}
-                                <span>Preview Image</span>
-                            </div>
-                        </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </>
-    );
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="xl"
+        static
+        className="modal-add-user"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add New User</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="row g-3">
+            <div className="col-md-6">
+              <label className="form-label">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label fclassName="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+            </div>
+            <div className="col-md-4">
+              <label className="form-label">Role</label>
+              <select
+                className="form-select"
+                onChange={(event) => setRole(event.target.value)}
+              >
+                <option value="USER">Role</option>
+                <option value="ADMIN">ADMIN</option>
+              </select>
+            </div>
+            <div className="col-md-12">
+              <label className="form-label label-upload" htmlFor="labelUpload">
+                <FaPlusCircle />
+                Upload File Image
+              </label>
+              <input
+                type="file"
+                hidden
+                id="labelUpload"
+                onChange={(event) => handleUploadImage(event)}
+              />
+              <div className="col-md-12 img-preview">
+                {previewImage ? (
+                  <img src={previewImage} />
+                ) : (
+                  <span>Preview Image</span>
+                )}
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
 }
 
-export default ModalCreateUser;
+export default ModalCreateUser
