6862f3c0beac9976152d114dfee938eb0729ff1a
Vinh Hoang Ngoc <vinhhoangngoc22@gmail.com>
2022-09-09 23:47:02 +0700

    Delete history

--- .history/src/redux/action/userAction_20220909232547.js
+++
@@ -1,7 +0,0 @@
-const FETCH_USER_LOGIN_SUCCESS = 'FETCH_USER_LOGIN_SUCCESS';
-export const doLogin = (data) => {
-    return {
-        type: FETCH_USER_LOGIN_SUCCESS,
-        payload: data
-    };
-};
